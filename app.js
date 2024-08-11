0d220e1fe950d287a7ee036f85396ccdadc9a5a2
Abishek Periasamy <126549517+pabishek61001@users.noreply.github.com>
2024-08-11 18:43:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         msg: "backend"
-    })
-})
+    });
+});
 
-app.listen(3000)
+app.listen(4000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
